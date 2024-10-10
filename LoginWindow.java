b2a06c4f50c4e45f3d7255e6a35df89ca144f30a
Juan Comella <juanpablocomella@gmail.com>
2024-10-10 12:24:32 +0200

    Changes to LoginWindow

--- LoginWindow.java
+++ LoginWindow.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class LoginWindow extends JFrame {
 
@@ -6,6 +8,24 @@ public class LoginWindow extends JFrame {
 
         setTitle("Inserta tu Usuario");
         setSize(700, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Crear panel principal
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        add(panel);
+
+        // Crear Titulo
+        JLabel titulo = new JLabel("Bienvenido a WEDO", JLabel.CENTER);
+        titulo.setFont(new Font("Monospaced", Font.PLAIN, 20));
+        titulo.setBounds(496/2,60, 204, 100);
+
+        panel.add(titulo);
+
+        // Crear Username input
+        JLabel username = new JLabel("Usuario");
+        JTextField usernameTF = new JTextField(20);
+
 
 
     }
