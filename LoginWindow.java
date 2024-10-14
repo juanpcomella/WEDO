01c11634276a1625b037934919edf4366913c639
Juan Comella <juanpablocomella@gmail.com>
2024-10-14 11:00:05 +0200

    Finished LoginWindow Prototype.

--- LoginWindow.java
+++ LoginWindow.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class LoginWindow extends JFrame {
@@ -22,10 +21,37 @@ public class LoginWindow extends JFrame {
 
         panel.add(titulo);
 
+        // Añadir Logo
+
         // Crear Username input
         JLabel username = new JLabel("Usuario");
-        JTextField usernameTF = new JTextField(20);
+        JTextField usernameTF = new JTextField(50);
 
+        username.setBounds(180, 170, 200, 20);
+        username.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        usernameTF.setBounds(300, 170, 250, 20);
+
+        panel.add(username);
+        panel.add(usernameTF);
+
+        // Crear el password input
+        JLabel password = new JLabel("Contraseña");
+        JPasswordField passwordTF = new JPasswordField(50);
+
+        password.setBounds(180, 210, 200, 20);
+        password.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        passwordTF.setBounds(300, 210, 250, 20);
+
+        panel.add(password);
+        panel.add(passwordTF);
+
+        // Crear el enter button
+        JButton login = new JButton("Login");
+        login.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        login.setSize(100, 50);
+        login.setLocation(350 - login.getWidth()/2, 250);
+
+        panel.add(login);
 
 
     }
