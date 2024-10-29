9a4a3e2118bfc1847c0d44c96f4c72964550b45f
Juan Comella <juanpablocomella@gmail.com>
2024-10-29 01:24:41 +0100

    Se creo la nueva rama devMainWindow para el desarrollo de VentanaPrincipal.

--- VentanaPrincipal.java
+++ VentanaPrincipal.java
@@ -1,5 +1,8 @@
 public class VentanaPrincipal {
     public static void main(String[] args) {
         System.out.println("Ventana Principal");
+
+        System.out.println("Hello World");
+        // Cree la nueva rama de devMainWindow para que en esta se desarrolle esta ventana.
     }
 }
