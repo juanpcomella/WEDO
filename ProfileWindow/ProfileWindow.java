d1e91fa7d769571bb90a67ff5a71bc4c6e4285ca
Juanpi <juanpablocomella@gmail.com>
2024-11-04 14:15:41 +0100

    New branch for profile window dev

---
+++ ProfileWindow/ProfileWindow.java
@@ -0,0 +1,20 @@
+package ProfileWindow;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProfileWindow extends JFrame{
+
+    public ProfileWindow() {
+
+        setTitle("WEDO - Profile");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+
+
+    }
+}
