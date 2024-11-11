25deb09c48b714e5e1f1ca53dba36967c6d83ae8
Juanpi <juanpablocomella@gmail.com>
2024-11-11 12:39:40 +0100

    Push avances ProfileWindow

--- ProfileWindow/ProfileWindow.java
+++ ProfileWindow/ProfileWindow.java
@@ -6,7 +6,6 @@ import java.awt.*;
 public class ProfileWindow extends JFrame{
 
     public ProfileWindow() {
-
         setTitle("WEDO - Profile");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -14,7 +13,28 @@ public class ProfileWindow extends JFrame{
         panel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
+        gbc.insets = new Insets(5,5,5,5);
+        gbc.fill = GridBagConstraints.BOTH;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0.5;
+        gbc.weighty = 1.0;
+
+        JPanel panelIzquierda = new JPanel();
+        panelIzquierda.setLayout(new GridBagLayout());
+
+        panelIzquierda.setBackground(Color.BLUE);
+        panelIzquierda.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panel.add(panelIzquierda, gbc);
 
 
+    }
+
+    public static void main(String[] args) {
+        ProfileWindow window = new ProfileWindow();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
