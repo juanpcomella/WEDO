3396d0f9eaf484887b6d9ae42698d5cc62ee4695
iker.gamboa <iker.gamboa@10.207.8.132>
2024-11-25 11:40:35 +0100

    empezando con la base de datos

--- VentanaRegistroDef.java
+++ VentanaRegistroDef.java
@@ -4,11 +4,19 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 
 public class VentanaRegistroDef extends JFrame {
 
     private static final long serialVersionUID = 1L;
+    String passwordBD;
+    String usernameBD;
+    String emailBD;
 
 	public VentanaRegistroDef() {
         setTitle("WEDO - Registro");
@@ -128,6 +136,7 @@ public class VentanaRegistroDef extends JFrame {
         registerButton.setFocusPainted(false);
         registerButton.setPreferredSize(new Dimension(150, 50));
         registerButton.setMaximumSize(new Dimension(150, 50));
+
         registerButton.addActionListener(new ActionListener() {
 			
 			@Override
@@ -138,6 +147,77 @@ public class VentanaRegistroDef extends JFrame {
 				
 			}
 		});
+        
+        registerButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String passwordUser = new String(passwordTF.getPassword());
+				String emailUser = emailTF.getText();
+				String usernameUser = usernameTF.getText();
+				if (usernameUser.isEmpty() || emailUser.isEmpty() || passwordUser.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Por favor rellena todos los datos.");
+                }else if (passwordTF != passwordConfTF) {
+                    JOptionPane.showMessageDialog(null,"Las contraseñas no son iguales.");
+                }else{
+                	passwordBD = passwordUser;
+    				usernameBD = usernameUser;
+    				emailBD = emailUser;
+                	
+                	try {
+            			Class.forName("org.sqlite.JDBC");
+            		} catch (ClassNotFoundException e1) {
+            			System.err.println("ERROR: Driver sqlite para JDBC no encontrado");
+            			return;
+            		}
+            		
+            		Connection connection = null;
+            		try {
+            			// Crear una conexión de BD
+            			connection = DriverManager.getConnection("jdbc:sqlite:src/BasesDeDatos/Usuarios");//a partir de los ultimo : es donde quieres que se guarden
+            			// Crear gestores de sentencias
+            			Statement statement = connection.createStatement();//crear consultas
+            			statement.setQueryTimeout(30);  // poner timeout 30 msg
+            			
+            			// Ejecutar sentencias SQL (Delete)
+            			statement.executeUpdate("drop table if exists usuario");
+            			
+            			// Ejecutar sentencias SQL (Update)
+            			statement.executeUpdate("create table usuario (username string, password string, email string)");
+            			
+            			//Ejecutar sentenciass SQL (Insert) insertar registros
+            			int res = statement.executeUpdate("insert into person values(1, 'leo')");
+            			res = statement.executeUpdate("insert into person values(2, 'yui')");
+            			
+
+            			System.out.println( "Número de registros insertados: "+res );
+
+            			
+            			// Ejecutar sentencias SQL (Select)
+            			ResultSet rs = statement.executeQuery("select * from person");
+            			while(rs.next()) {
+            				// Leer el resultset
+            				System.out.println("name = " + rs.getString("name"));
+            				System.out.println("id = " + rs.getInt("id"));
+            			}
+            		} catch(SQLException e1) {
+            			System.err.println(e1.getMessage());
+            		} finally {
+            			try {
+            				if(connection != null)
+            					connection.close();
+            			} catch(SQLException e1) {
+            				// Cierre de conexión fallido
+            				System.err.println(e1);
+            			}
+            	}
+                    JOptionPane.showMessageDialog(null, "Cuenta Creada! \nBienvenido a WEDO!");
+
+                }
+
+			}       	
+        });
+        
         gbc.gridx = 0;
         gbc.gridy = 10;
         gbc.gridwidth = 2;
@@ -152,31 +232,16 @@ public class VentanaRegistroDef extends JFrame {
         users.put("anderorma", "contraseña");
         users.put("ikergamboa", "98765");
 
-        registerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String username = usernameTF.getText();
-                String email = emailTF.getText();
-                String password = new String(passwordTF.getPassword());
-
-
-                if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Por favor rellena todos los datos.");
-                } else if (users.containsKey(username)) {
-                    JOptionPane.showMessageDialog(null, "Ese usuario ya existe");
-                }else if (passwordTF != passwordConfTF) {
-                    JOptionPane.showMessageDialog(null,"Las contraseñas no son iguales.");
-                }else{
-                    JOptionPane.showMessageDialog(null, "Cuenta Creada! \nBienvenido a WEDO!");
-                }
-            }
-        });
+ 
 
         panel.add(datos, BorderLayout.CENTER);
 
     }
 
     public static void main(String[] args) {
+//		// Carga el sqlite-JDBC driver usando el cargador de clases
+		
+		
         VentanaRegistroDef window = new VentanaRegistroDef();
         window.setResizable(false);
         window.setLocationRelativeTo(null);
