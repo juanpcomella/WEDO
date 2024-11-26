51c659b3f94cfce9e8e5622395c38edee63b231c
iker.gamboa <iker.gamboa@SS23-5008E47600>
2024-11-26 14:23:57 +0100

    meter nuevos usuarios

--- BaseDeDatos/BDs.java
+++ BaseDeDatos/BDs.java
@@ -1,6 +1,7 @@
 package BaseDeDatos;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class BDs {
 	public static void crearTabla() {
@@ -13,7 +14,7 @@ public class BDs {
 		Connection connection = null;
 		try {
 			// Crear una conexión de BD
-			connection = DriverManager.getConnection("jdbc:sqlite:BaseDeDatos/usuarios");//a partir de los ultimo : es donde quieres que se guarden
+			connection = DriverManager.getConnection("jdbc:sqlite:BaseDeDatos/usuario2");//a partir de los ultimo : es donde quieres que se guarden
 			// Crear gestores de sentencias
 			Statement statement = connection.createStatement();//crear consultas
 			statement.setQueryTimeout(30);  // poner timeout 30 msg
@@ -22,7 +23,7 @@ public class BDs {
 //			statement.executeUpdate("drop table if exists person");
 			
 			// Ejecutar sentencias SQL (Update)
-			statement.executeUpdate("create table if not exists usuario (username string, password string, email string)");
+			statement.executeUpdate("create table if not exists usuarios (username string, password string, email string)");
 
 		} catch(SQLException e) {
 			System.err.println(e.getMessage());
@@ -46,12 +47,20 @@ public class BDs {
 		Connection connection = null;
 		try {
 			// Crear una conexión de BD
-			connection = DriverManager.getConnection("jdbc:sqlite:BaseDeDatos/usuarios");//a partir de los ultimo : es donde quieres que se guarden
+			connection = DriverManager.getConnection("jdbc:sqlite:BaseDeDatos/usuario2");//a partir de los ultimo : es donde quieres que se guarden
 			// Crear gestores de sentencias
 			Statement statement = connection.createStatement();//crear consultas
 			statement.setQueryTimeout(30);  // poner timeout 30 msg
 			
-			statement.executeUpdate("insert into usuario values(usuario, contraseña, email)");
+			String sql = "INSERT INTO usuarios (username, password, email) VALUES (?, ?, ?)";
+
+            // Preparar la consulta
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            // Asignar los valores de las variables a los parámetros
+            preparedStatement.setString(1, usuario);     
+            preparedStatement.setString(2, contraseña);  
+            preparedStatement.setString(3, email);
 
 		} catch(SQLException e) {
 			System.err.println(e.getMessage());
@@ -64,6 +73,77 @@ public class BDs {
 				System.err.println(e);
 			}
 	}
+	}
+	public static ArrayList<String> usuarioExistente() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			System.err.println("ERROR: Driver sqlite para JDBC no encontrado");
+		}
+		Connection connection = null;
+		ArrayList<String> nombresUsuario = new ArrayList<>();
+
+		try {
+			// Crear una conexión de BD
+			connection = DriverManager.getConnection("jdbc:sqlite:BaseDeDatos/usuario2");//a partir de los ultimo : es donde quieres que se guarden
+			// Crear gestores de sentencias
+			Statement statement = connection.createStatement();//crear consultas
+			statement.setQueryTimeout(30);  // poner timeout 30 msg
+			
+			String sql = "SELECT username FROM usuarios";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            // Recorrer los resultados y añadirlos al ArrayList
+            while (resultSet.next()) {
+                nombresUsuario.add(resultSet.getString("username"));
+            }
+
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if(connection != null)
+					connection.close();
+			} catch(SQLException e) {
+				// Cierre de conexión fallido
+				System.err.println(e);
+			}
+	}
+	return nombresUsuario;
+	}
+	public static ArrayList<String> contraseñaExistente() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			System.err.println("ERROR: Driver sqlite para JDBC no encontrado");
+		}
+		Connection connection = null;
+		ArrayList<String> contraseñaExistente = new ArrayList<>();
+
+		try {
+			// Crear una conexión de BD
+			connection = DriverManager.getConnection("jdbc:sqlite:BaseDeDatos/usuario2");//a partir de los ultimo : es donde quieres que se guarden
+			// Crear gestores de sentencias
+			Statement statement = connection.createStatement();//crear consultas
+			statement.setQueryTimeout(30);  // poner timeout 30 msg
+			
+			ResultSet rs = statement.executeQuery("select * from usuarios");
+			while(rs.next()) {
+				contraseñaExistente.add(rs.getString("password"));
+			}
+
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if(connection != null)
+					connection.close();
+			} catch(SQLException e) {
+				// Cierre de conexión fallido
+				System.err.println(e);
+			}
+	}
+	return contraseñaExistente;
 	}
 	
 
--- VentanaRegistroDef.java
+++ VentanaRegistroDef.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import javax.swing.border.Border;
 
+import BaseDeDatos.BDs;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -136,82 +138,41 @@ public class VentanaRegistroDef extends JFrame {
         registerButton.setFocusPainted(false);
         registerButton.setPreferredSize(new Dimension(150, 50));
         registerButton.setMaximumSize(new Dimension(150, 50));
-
-        registerButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				VentanaEmpezarCuestionario nuevaVentana = new VentanaEmpezarCuestionario();
-				nuevaVentana.setVisible(true);
-				dispose();
-				
-			}
-		});
         
         registerButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String passwordUser = new String(passwordTF.getPassword());
+				String passwordConfUser = new String(passwordConfTF.getPassword());
 				String emailUser = emailTF.getText();
 				String usernameUser = usernameTF.getText();
 				if (usernameUser.isEmpty() || emailUser.isEmpty() || passwordUser.isEmpty()) {
                     JOptionPane.showMessageDialog(null, "Por favor rellena todos los datos.");
-                }else if (passwordTF != passwordConfTF) {
+                }else if (!passwordUser.equals(passwordConfUser)) {
                     JOptionPane.showMessageDialog(null,"Las contraseñas no son iguales.");
-                }else{
+                }
+//                else if(BDs.usuarioExistente().contains(usernameUser)){
+//                	JOptionPane.showMessageDialog(null,"Nombre de usuario existente");
+//                }else if(BDs.contraseñaExistente().contains(passwordUser)){
+//                	JOptionPane.showMessageDialog(null,"Nombre de usuario existente");
+//                }
+                else {
                 	passwordBD = passwordUser;
     				usernameBD = usernameUser;
     				emailBD = emailUser;
+    				
                 	
-                	try {
-            			Class.forName("org.sqlite.JDBC");
-            		} catch (ClassNotFoundException e1) {
-            			System.err.println("ERROR: Driver sqlite para JDBC no encontrado");
-            			return;
-            		}
-            		
-            		Connection connection = null;
-            		try {
-            			// Crear una conexión de BD
-            			connection = DriverManager.getConnection("jdbc:sqlite:src/BasesDeDatos/Usuarios");//a partir de los ultimo : es donde quieres que se guarden
-            			// Crear gestores de sentencias
-            			Statement statement = connection.createStatement();//crear consultas
-            			statement.setQueryTimeout(30);  // poner timeout 30 msg
-            			
-            			// Ejecutar sentencias SQL (Delete)
-            			statement.executeUpdate("drop table if exists usuario");
-            			
-            			// Ejecutar sentencias SQL (Update)
-            			statement.executeUpdate("create table usuario (username string, password string, email string)");
-            			
-            			//Ejecutar sentenciass SQL (Insert) insertar registros
-            			int res = statement.executeUpdate("insert into person values(1, 'leo')");
-            			res = statement.executeUpdate("insert into person values(2, 'yui')");
-            			
-
-            			System.out.println( "Número de registros insertados: "+res );
-
-            			
-            			// Ejecutar sentencias SQL (Select)
-            			ResultSet rs = statement.executeQuery("select * from person");
-            			while(rs.next()) {
-            				// Leer el resultset
-            				System.out.println("name = " + rs.getString("name"));
-            				System.out.println("id = " + rs.getInt("id"));
-            			}
-            		} catch(SQLException e1) {
-            			System.err.println(e1.getMessage());
-            		} finally {
-            			try {
-            				if(connection != null)
-            					connection.close();
-            			} catch(SQLException e1) {
-            				// Cierre de conexión fallido
-            				System.err.println(e1);
-            			}
-            	}
+                	BDs.crearTabla();
+                	BDs.insertarElementos(usernameBD, passwordBD, emailBD);
+                	for (int i = 0; i < BDs.usuarioExistente().size(); i++) {
+    		            System.out.println(BDs.usuarioExistente().get(i));
+    				}
+    				System.out.println(BDs.usuarioExistente().size());
                     JOptionPane.showMessageDialog(null, "Cuenta Creada! \nBienvenido a WEDO!");
+                    VentanaEmpezarCuestionario nuevaVentana = new VentanaEmpezarCuestionario();
+    				nuevaVentana.setVisible(true);
+    				dispose();
 
                 }
 
