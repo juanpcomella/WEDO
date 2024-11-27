fcb65675bf9ded76a68951363fba6f9359ccf291
Juanpi <juanpablocomella@gmail.com>
2024-11-27 14:19:05 +0100

    cambios navbar y mainwindow

--- MainWindow/MainWindow.java
+++ MainWindow/MainWindow.java
@@ -5,30 +5,44 @@ import java.awt.*;
 import java.time.LocalDate;
 
 public class MainWindow extends JFrame {
-    
+
     public MainWindow() {
         setTitle("WEDO");
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
         add(panel);
-        
+
         JPanel panelNorte = new JPanel();
+        panelNorte.setLayout(new BorderLayout());
+
         JPanel panelCentro = new JPanel();
         panelCentro.setLayout(new BorderLayout());
-        
+
+        Navbar navbar = new Navbar();
+        int navbarHeight = (int) (getHeight() * 0.1);
+        navbar.setPreferredSize(new Dimension(getWidth(), navbarHeight));
+        panelNorte.add(navbar, BorderLayout.NORTH);
+
         Calendario calendario = new Calendario(LocalDate.now().getYear(), LocalDate.now().getMonthValue());
-        
         panelCentro.add(calendario, BorderLayout.CENTER);
-        
+
         panel.add(panelNorte, BorderLayout.NORTH);
         panel.add(panelCentro, BorderLayout.CENTER);
-        
+
+        addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                int dynamicHeight = (int) (getHeight() * 0.1);
+                navbar.setPreferredSize(new Dimension(getWidth(), dynamicHeight));
+                navbar.revalidate();
+            }
+        });
+
         setVisible(true);
     }
-        
+
     public static void main(String[] args) {
         MainWindow window = new MainWindow();
         window.setVisible(true);
--- MainWindow/Navbar.java
+++ MainWindow/Navbar.java
@@ -1,4 +1,27 @@
 package MainWindow;
 
-public class Navbar {
+import javax.swing.*;
+import java.awt.*;
+
+public class Navbar extends JPanel {
+
+    public Navbar() {
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        setBackground(Color.LIGHT_GRAY);
+
+        JButton hamburgerMenu = new JButton("≡");
+        hamburgerMenu.setFocusable(false);
+        hamburgerMenu.setFont(new Font("Arial", Font.BOLD, 30));
+        hamburgerMenu.setPreferredSize(new Dimension(65, 65));
+
+        JTextField searchTF = new JTextField(20);
+
+        JButton aboutButton = new JButton("\uD83D\uDD0E");
+        JButton contactButton = new JButton("Contact");
+
+        add(hamburgerMenu);
+        add(searchTF);
+        add(aboutButton);
+        add(contactButton);
+    }
 }
