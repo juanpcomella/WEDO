eb67f0f2755196c67e00ab2cfe3c7d2ae9de5ce9
adrian.baz <adrian.baz@192.168.1.89>
2024-11-29 19:33:10 +0100

    errores solucionados: falta que el label comprado se mantenga

--- VentanaTienda/VentanaTienda.java
+++ VentanaTienda/VentanaTienda.java
@@ -1,23 +1,29 @@
 package VentanaTienda;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.security.KeyStore.TrustedCertificateEntry;
 import java.util.HashMap;
 import java.util.Map;
 
 public class VentanaTienda extends JFrame {
-private int hoveredColumn = -1;
-private int hoveredRow = -1;
-private int selectedRow = -1;
-private int selectedColumn = -1;
-private Map <Point, Boolean> estadoCeldas = new HashMap<>();
-private boolean compra;
+
+    private int hoveredColumn = -1;
+    private int hoveredRow = -1;
+    private int selectedRow = -1;
+    private int selectedColumn = -1;
+    private Map<Point, Boolean> estadoCeldas = new HashMap<>();
+    private boolean compra;
 
     public VentanaTienda() {
         // Configuración de la ventana principal
@@ -25,10 +31,9 @@ private boolean compra;
         setSize(800, 600);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-       
-        //marcar el estado de las celdas
-        Map<Point, Boolean> estadoCeldas = new HashMap<>();
 
+        // Marcar el estado de las celdas
+        Map<Point, Boolean> estadoCeldas = new HashMap<>();
 
         // TabbedPane para las secciones
         JTabbedPane tabbedPane = new JTabbedPane();
@@ -36,98 +41,68 @@ private boolean compra;
         // Panel para los iconos
         JPanel panelIcono = new JPanel(new BorderLayout());
         tabbedPane.addTab("Iconos de perfil", panelIcono);
-       
-        //Panel para l
 
         // Modelo de la tabla de iconos
         DefaultTableModel modeloIcono = new DefaultTableModel(new Object[]{"Icono", "Precio"}, 0) {
-        public boolean isCellEditable (int row, int column) {
-        if (column ==0) {
-        return false;
-        } else {
-        return true;
-        }
-        }
+            public boolean isCellEditable(int row, int column) {
+                return column != 0;
+            }
         };
+
         JTable iconoT = new JTable(modeloIcono);
-       
         iconoT.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                int row = iconoT.rowAtPoint(e.getPoint());
+                int column = iconoT.columnAtPoint(e.getPoint());
 
-@Override
-public void mouseMoved(MouseEvent e) {
-int row = iconoT.rowAtPoint(e.getPoint());
-int column = iconoT.columnAtPoint(e.getPoint());
-if (column ==1) {
-hoveredRow = row;
-hoveredColumn = column;
-} else {
-hoveredRow = -1;
-hoveredColumn = -1;
-}
-iconoT.repaint();
-// TODO Auto-generated method stub
+                if (column == 1) {
+                    hoveredRow = row;
+                    hoveredColumn = column;
+                } else {
+                    hoveredRow = -1;
+                    hoveredColumn = -1;
+                }
 
-}
+                iconoT.repaint();
+            }
 
-@Override
-public void mouseDragged(MouseEvent e) {
-// TODO Auto-generated method stub
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                // Método generado automáticamente
+            }
+        });
 
-}
-});
         Point celda = new Point(hoveredRow, hoveredColumn);
-       
-        //ocultamos los tableheaders:
+
+        // Ocultamos los tableheaders
         iconoT.getTableHeader().setVisible(false);
-        iconoT.getTableHeader().setPreferredSize(new Dimension(0,0));
+        iconoT.getTableHeader().setPreferredSize(new Dimension(0, 0));
         iconoT.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         iconoT.setRowHeight(100);
+
         panelIcono.add(new JScrollPane(iconoT), BorderLayout.CENTER);
-       
-        //DINERO ALMACENADO AQUI, LUEGO SE HARÁ UN PARSEINT PARA CONVERTIR
+
+        // Dinero almacenado aquí, luego se hará un parseInt para convertir
         String dinero = "0";
-        //Aqui el parseint
+
+        // Parseo de dinero
         int money = Integer.parseInt(dinero);
 
-
-        // Cargar una imagen y redimensionarla
+        // Cargar imágenes y redimensionarlas
         ImageIcon icon1 = new ImageIcon("imagenes/Imagen1.png");
         Image img = icon1.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
         icon1 = new ImageIcon(img);
-       
-        ImageIcon logoOso = new ImageIcon("imagenes/logoOso.png");
-        Image imgOso = logoOso.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        logoOso = new ImageIcon(imgOso);
 
-        ImageIcon logoPerro = new ImageIcon("imagenes/logoPerro.png");
-        Image imgPerro = logoPerro.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        logoPerro = new ImageIcon(imgPerro);
+        ImageIcon logoOso = cargarImagen("imagenes/logoOso.png", 80, 80);
+        ImageIcon logoPerro = cargarImagen("imagenes/logoPerro.png", 80, 80);
+        ImageIcon logoPanda = cargarImagen("imagenes/logoPanda.png", 80, 80);
+        ImageIcon logoUnicornio = cargarImagen("imagenes/logoUnicornio.png", 80, 80);
+        ImageIcon perfilMonje = cargarImagen("imagenes/perfilMonje.png", 80, 80);
+        ImageIcon logoPlayaPalmeras = cargarImagen("imagenes/logoPlayaPalmeras.png", 80, 80);
+        ImageIcon logoHomer = cargarImagen("imagenes/logoHomer.png", 80, 80);
 
-        ImageIcon logoPanda = new ImageIcon("imagenes/logoPanda.png");
-        Image imgPanda = logoPanda.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        logoPanda = new ImageIcon(imgPanda);
-
-        ImageIcon logoUnicornio = new ImageIcon("imagenes/logoUnicornio.png");
-        Image imgUnicornio = logoUnicornio.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        logoUnicornio = new ImageIcon(imgUnicornio);
-
-        ImageIcon perfilMonje = new ImageIcon("imagenes/perfilMonje.png");
-        Image imgMonje = perfilMonje.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        perfilMonje = new ImageIcon(imgMonje);
-
-        ImageIcon logoPlayaPalmeras = new ImageIcon("imagenes/logoPlayaPalmeras.png");
-        Image imgPlayaPalmeras = logoPlayaPalmeras.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        logoPlayaPalmeras = new ImageIcon(imgPlayaPalmeras);
-
-        ImageIcon logoHomer = new ImageIcon("imagenes/logoHomer.png");
-        Image imgHomer = logoHomer.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-        logoHomer = new ImageIcon(imgHomer);
-
-        ImageIcon icon2 = new ImageIcon("imagenes/coin.jpg");
-        img = icon2.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-        icon2 = new ImageIcon(img);
-       
-       
+        ImageIcon icon2 = cargarImagen("imagenes/coin.jpg", 50, 50);
 
         // Agregar ejemplos de filas con un icono y precio
         modeloIcono.addRow(new Object[]{logoHomer, new Object[]{85, icon2}});
@@ -137,73 +112,48 @@ public void mouseDragged(MouseEvent e) {
         modeloIcono.addRow(new Object[]{logoPlayaPalmeras, new Object[]{140, icon2}});
         modeloIcono.addRow(new Object[]{perfilMonje, new Object[]{300, icon2}});
 
-       
-
         // Renderizador para la columna de "Precio" (JPanel con número e imagen)
         iconoT.getColumnModel().getColumn(1).setCellRenderer(new DefaultTableCellRenderer() {
             @Override
-            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-                                                           int row, int column) {
-                //crear un panel para la celda
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-                if(selectedRow != -1 && row == selectedRow) {
-                JLabel comprado = new JLabel ("Comprado");
-                return comprado; //CENTRALIZAR VARIABLES E INICIALIZAR VARIABLES DE SELECCION PARA SABER LA FILA QUE ESTA SELECCIONADA
-               
+
+                if (selectedRow != -1 && row == selectedRow && compra) {
+                    JLabel comprado = new JLabel("Comprado");
+                	iconoT.setValueAt(comprado, selectedRow, 1);
+                    return comprado;
                 }
-               
+
                 if (column == hoveredColumn && row == hoveredRow) {
-                JButton comprarB = new JButton("Comprar");
-                return comprarB;
+                    JButton comprarB = new JButton("Comprar");
+                    return comprarB;
                 } else {
-                    //verificar que el valor sea un Object[]
                     if (value instanceof Object[]) {
                         Object[] cellData = (Object[]) value;
-
-                        // Agregar el número al panel
                         JLabel numberLabel = new JLabel(String.valueOf(cellData[0]));
                         panel.add(numberLabel);
-                       
 
-                        // Agregar la imagen al panel
                         if (cellData[1] instanceof Icon) {
-                        Image img = ((ImageIcon) cellData[1]).getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-                            JLabel iconLabel = new JLabel((Icon)cellData[1]);
+                            JLabel iconLabel = new JLabel((Icon) cellData[1]);
                             panel.add(iconLabel);
                         }
+                    }
                 }
-
-
-// TODO Auto-generated method stub
-
-
-                //asegurar colores de selección
-               
-                }
-
                 return panel;
             }
         });
-       
+
         JLabel dineroL = new JLabel(icon2);
-       
         JPanel panelNorteIcono = new JPanel();
-       
         JLabel stringDinero = new JLabel(dinero);
-       
         panelNorteIcono.add(dineroL);
         panelNorteIcono.add(stringDinero);
-       
-        panelIcono.add(panelNorteIcono,BorderLayout.NORTH);
-       
-       
+        panelIcono.add(panelNorteIcono, BorderLayout.NORTH);
 
-       
-        //renderizador para la columna de "Icono" (solo imagen)
+        // Renderizador para la columna de "Icono" (solo imagen)
         iconoT.getColumnModel().getColumn(0).setCellRenderer(new DefaultTableCellRenderer() {
             @Override
-            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-                                                           int row, int column) {
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 if (value instanceof Icon) {
                     return new JLabel((Icon) value);
                 }
@@ -211,76 +161,81 @@ public void mouseDragged(MouseEvent e) {
             }
         });
 
-//FUNCIONALIDAD DEL BOTON
-class ButtonCellEditor extends AbstractCellEditor implements TableCellEditor {
-   private JPanel panel;
-   private JButton button;
-   private Object valorOriginal;
-   private boolean comprado;
+        // Funcionalidad del botón
+        class ButtonCellEditor extends AbstractCellEditor implements TableCellEditor {
+            private JPanel panel;
+            private JButton button;
+            private Object valorOriginal;
+            private JLabel comprado;
+            private Object componente;
 
-   public ButtonCellEditor() {
-       //crear el botón y configurarlo
-       button = new JButton("Comprar");
-       button.addActionListener(e -> {
-           // Acción del botón (mostrar diálogo, etc.)
-           int respuesta = JOptionPane.showConfirmDialog(null, "¿Desea comprar?", "Comprar", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
-           
-           if (respuesta == JOptionPane.YES_OPTION) {
-               System.out.println("Compra realizada.");
-               estadoCeldas.put(celda, true);
-               compra = true;
-               
-               panel.repaint();
-               
-               
-           } else {
-               System.out.println("Compra cancelada.");
-               compra = false;
-               estadoCeldas.put(celda, false);
-               
-           }
+            public ButtonCellEditor() {
+                button = new JButton("Comprar");
+                button.addActionListener(e -> {
+                    int respuesta = JOptionPane.showConfirmDialog(null, "¿Desea comprar?", "Comprar", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+                    if (respuesta == JOptionPane.YES_OPTION) {
+                        comprado = new JLabel("comprado");
+                        componente = iconoT.getValueAt(selectedRow, 1);
+                        if (componente ==comprado) {
+                        	JOptionPane.showMessageDialog(null, "Ya ha comprado este objeto", "Error", JOptionPane.ERROR_MESSAGE);
+                            iconoT.setValueAt(comprado, selectedRow, 1);
 
-           fireEditingStopped(); // Finalizar edición después del clic
-       });
+                        }
+                        System.out.println("Compra realizada.");
+                        estadoCeldas.put(celda, true);
+                        compra = true;
+                        iconoT.setValueAt(comprado, selectedRow, 1);
+                        panel.repaint();
+                    } else {
+                        System.out.println("Compra cancelada.");
+                        compra = false;
+                        estadoCeldas.put(celda, false);
+                    }
+                    fireEditingStopped();
+                });
 
-       // Crear el panel y agregar el botón
-       panel = new JPanel(new BorderLayout());
-       panel.add(button, BorderLayout.CENTER);
-   }
+                panel = new JPanel(new BorderLayout());
+                panel.add(button, BorderLayout.CENTER);
+            }
 
-   @Override
-   public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-    selectedRow = row;
-    button.setText("Comprar");
-    valorOriginal = value;
-return panel;
-   }
-   @Override
-   public Object getCellEditorValue() {
-   
-       return valorOriginal; // Devuelve un valor si es necesario
-   }
-}
-iconoT.getColumnModel().getColumn(1).setCellEditor(new ButtonCellEditor());
+            @Override
+            public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+                selectedRow = row;
+                button.setText("Comprar");
+                valorOriginal = value;
+                    return panel;
 
+                
+            }
 
+            @Override
+            public Object getCellEditorValue() {
+                return valorOriginal;
+            }
+        }
 
-        //panel para los apodos
+        iconoT.getColumnModel().getColumn(1).setCellEditor(new ButtonCellEditor());
+
+        // Panel para los apodos
         JPanel panelApodos = new JPanel();
         tabbedPane.addTab("Apodos", panelApodos);
 
-        //panel para la temática
+        // Panel para la temática
         JPanel panelTematica = new JPanel();
         tabbedPane.addTab("Temática", panelTematica);
 
-        //agregar el tabbedPane al framee
+        // Agregar el tabbedPane al frame
         add(tabbedPane);
-
         setVisible(true);
     }
 
+    private ImageIcon cargarImagen(String ruta, int ancho, int alto) {
+        ImageIcon icono = new ImageIcon(ruta);
+        Image img = icono.getImage().getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
+        return new ImageIcon(img);
+    }
+
     public static void main(String[] args) {
-   
         VentanaTienda ventana = new VentanaTienda();
         ventana.setVisible(true);
     }
