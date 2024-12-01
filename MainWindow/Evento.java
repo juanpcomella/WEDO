030bb7e2b518eae2a6a154889b023ace3a7ca425
ander.ormazabal <ander.ormazabal@192.168.0.42>
2024-12-01 11:08:37 +0100

    varios cambios

--- MainWindow/Evento.java
+++ MainWindow/Evento.java
@@ -5,59 +5,32 @@ import java.time.LocalDate;
 
 
 public class Evento {
-
     private String nombre;
-
     private String descripcion;
-
     private Categorias categoria;
-
     private LocalDate fecha;
 
-
-
     public Evento(String nombre, String descripcion, Categorias categoria, LocalDate fecha) {
-
         this.nombre = nombre;
-
         this.descripcion = descripcion;
-
         this.categoria = categoria;
-
         this.fecha = fecha;
-
     }
-
-
 
     public String getNombre() {
-
         return nombre;
-
     }
-
-
 
     public String getDescripcion() {
-
         return descripcion;
-
     }
-
-
 
     public Categorias getCategoria() {
-
         return categoria;
-
     }
 
-
-
     public LocalDate getFecha() {
-
         return fecha;
-
     }
 
 }
