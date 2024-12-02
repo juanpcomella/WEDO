1e91b1b8cfc069533f73a27a94b802e86e194ee3
iker.gamboa <iker.gamboa@SS23-5008E47600>
2024-12-02 20:38:28 +0100

    nuevos metodos creados en BDs: crear la tabla de eventos e insertar elementos en esta

--- BaseDeDatos/BDs.java
+++ BaseDeDatos/BDs.java
@@ -3,8 +3,10 @@ package BaseDeDatos;
 import java.sql.*;
 import java.util.ArrayList;
 
+import org.w3c.dom.Text;
+
 public class BDs {
-	public static void crearTabla() {
+	public static void crearTablaUsuarios() {
 		try {
 			Class.forName("org.sqlite.JDBC");
 		} catch (ClassNotFoundException e) {
@@ -23,7 +25,7 @@ public class BDs {
 //			statement.executeUpdate("drop table if exists person");
 			
 			// Ejecutar sentencias SQL (Update)
-			statement.executeUpdate("create table if not exists usuarios (username string, password string, email string, calendarioPersonal text)");
+			statement.executeUpdate("create table if not exists usuarios (username string, password string, email string)");
 
 		} catch(SQLException e) {
 			System.err.println(e.getMessage());
@@ -340,5 +342,85 @@ public class BDs {
 	}
 		return correoE;
 	}
+	//-----------------------------------------------------------------------------------------------------------------------------------------------------
+	//empezamos a trabajar con los eventos para cada usuario
+	public static void crearTablaEventos() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			System.err.println("ERROR: Driver sqlite para JDBC no encontrado");
+			return;
+		}
+		Connection connection = null;
+		try {
+			// Crear una conexión de BD
+			connection = DriverManager.getConnection("jdbc:sqlite:BaseDeDatos/usuario2");//a partir de los ultimo : es donde quieres que se guarden
+			// Crear gestores de sentencias
+			Statement statement = connection.createStatement();//crear consultas
+			statement.setQueryTimeout(30);  // poner timeout 30 msg
+			
+			// Ejecutar sentencias SQL (Delete)
+//			statement.executeUpdate("drop table if exists person");
+			
+			// Ejecutar sentencias SQL (Update)
+			statement.executeUpdate("create table if not exists eventos (username string, nombreEvento string, descripcionEv string, categoriaEv string, fechaEv string, "
+					+ "horaInicioEv string, horaFinEv string, todoElDiaEv boolean)");
 
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if(connection != null)
+					connection.close();
+			} catch(SQLException e) {
+				// Cierre de conexión fallido
+				System.err.println(e);
+			}
+	}
+	}
+	
+	public static void insertarEventos(String usuario, String nombre, String descripcion, String categoria, String fecha, String horaInicio, String horaFin, boolean todoElDia) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			System.err.println("ERROR: Driver sqlite para JDBC no encontrado");
+			return;
+		}
+		Connection connection = null;
+		try {
+			// Crear una conexión de BD
+			connection = DriverManager.getConnection("jdbc:sqlite:BaseDeDatos/usuario2");//a partir de los ultimo : es donde quieres que se guarden
+			// Crear gestores de sentencias
+			Statement statement = connection.createStatement();//crear consultas
+			statement.setQueryTimeout(30);  // poner timeout 30 msg
+			
+			String sql = "insert into eventos (username, nombreEvento, descripcionEv, categoriaEv, fechaEv, horaInicioEv, horaFinEv, todoElDiaEv) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            // Preparar la consulta
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            // Asignar los valores de las variables a los parámetros
+            preparedStatement.setString(1, usuario);     
+            preparedStatement.setString(2, nombre);  
+            preparedStatement.setString(3, descripcion);
+            preparedStatement.setString(4, categoria);
+            preparedStatement.setString(5, fecha);
+            preparedStatement.setString(6, horaInicio);
+            preparedStatement.setString(7, horaFin);
+            preparedStatement.setBoolean(8, todoElDia);
+          
+            preparedStatement.executeUpdate();
+
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if(connection != null)
+					connection.close();
+			} catch(SQLException e) {
+				// Cierre de conexión fallido
+				System.err.println(e);
+			}
+	}
+	}
 }
